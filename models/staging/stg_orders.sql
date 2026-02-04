f632440488cc3c62efd4cc31dc3a1ea38e61992b
jdewet03@gmail.com <jdewet03@gmail.com>
2026-02-04 11:21:46 +0000

    fix stg model

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -25,7 +25,7 @@ renamed as (
         {{ cents_to_dollars('order_total') }} as order_total,
 
         ---------- timestamps
-        cast(ordered_at as date) as order_date
+        cast(cast(ordered_at as datetime) as date) as order_date
 
     from source
 
